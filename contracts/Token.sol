03794fe112b7967f09ef0d9573fe56941932c4ca
Hariharan <hariharan@localhost.localdomain>
2018-09-26 22:46:28 +0530

    Initial implementation of ERC-20 interface. No events.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,23 +5,48 @@ contract Token {
     string public constant name = "Stimulon";
     string public constant symbol = "STM";
 
-    mapping(address => uint) public balanceOf;
+    struct UserData {
+        //Maps each user to the value limit
+        mapping(address => uint) access;
+        uint balance;
+    }
+
+    mapping(address => UserData) public balances;
 
     constructor() public {
-        balanceOf[msg.sender] = totalSupply;
+        balances[msg.sender] = UserData({balance: totalSupply});
+    }
+
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner].balance;
     }
 
     function transfer(address to, uint tokens) public returns (bool success) {
-        require(balanceOf[msg.sender] > tokens, "Insufficient balance with the main account");
-        balanceOf[to] += tokens;
-        balanceOf[msg.sender] -= tokens;
+        require(balances[msg.sender].balance >= tokens, "Insufficient balance with the main account");
+        balances[to].balance += tokens;
+        balances[msg.sender].balance -= tokens;
         return true;
     }
 
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        require(balanceOf[from] > tokens, "Insufficient balance with the sender");
-        balanceOf[to] += tokens;
-        balanceOf[from] -= tokens;
+        require(balances[from].balance >= tokens, "Insufficient balance with the sender");
+        require(balances[from].access[msg.sender] >= tokens, "Not allowed to access this account");
+        balances[to].balance += tokens;
+        balances[from].balance -= tokens;
+        balances[from].access[msg.sender] -= tokens;
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        balances[msg.sender].access[_spender] = _value;
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return balances[_owner].access[_spender];
+    }
+
+    // function getSpenderAndValue(address _owner) public returns (address _spender, uint _value) {
+    //     return (balances[_owner].spender, balances[_owner].value);
+    // }
 }
--- test/TokenTest.sol
+++ test/TokenTest.sol
@@ -31,4 +31,14 @@ contract TokenTest {
         uint balance = token.balanceOf(0x54478ED1AC5b0ea8E174D4d8b10f7223FCE21308);
         Assert.equal(balance, 20, "Transfer STM tokens");
     }
+
+    //Just for testing the function an extra sender argument
+    // function testApprove() public {
+    //     address _expectedSpender = 0xA505c97094674b7befC266baeD20e5D9B5A35d16;
+    //     uint _expectedValue = 10;
+    //     token.approve(msg.sender, _expectedSpender, _expectedValue);
+    //     (address spender, uint value) = token.getSpenderAndValue(msg.sender);
+    //     Assert.equal(spender, _expectedSpender, "Approved spender");
+    //     Assert.equal(value, _expectedValue, "Approved value to be spent by spender");
+    // }
 }
